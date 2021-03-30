1fc7d9d35861148a0b54d9651a5db5aa50cb9899
Siew Chin Lim <elly.siew.chin.lim@intel.com>
2021-03-30 23:48:31 +0800

    configs: defconfig: Load OS via FIT image in Stratix 10 and Agilex NAND boot
    Update to load OS via FIT image in Stratix 10 and Agilex NAND boot flow.
    And, enable FIT signature checking with crc32 algorithm.
    
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

--- include/configs/socfpga_soc64_common.h
+++ include/configs/socfpga_soc64_common.h
@@ -228,6 +228,10 @@
 	"scriptfile=u-boot.scr\0" \
 	"fatscript=if fatload mmc 0:1 ${scriptaddr} ${scriptfile};" \
 		   "then source ${scriptaddr}:script; fi\0" \
+	"nandfitboot=setenv bootargs " CONFIG_BOOTARGS \
+			" root=${nandroot} rw rootwait rootfstype=jffs2; " \
+			"bootm ${loadaddr}\0" \
+	"nandfitload=nand read ${loadaddr} kernel\0" \
 	"socfpga_legacy_reset_compat=1\0" \
 	"rsu_status=rsu dtb; rsu display_dcmf_version; "\
 		"rsu display_dcmf_status; rsu display_max_retry\0" \
