3a9567b27159eb633f81d8bc718377593aa1b9f3
Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>
2025-07-11 20:47:55 +0800

    misc: socfpga_secreg: Enable register settings in DTS
    Enable register settings from device tree in SPL, which require high
    privilege access like firewall registers. This also provides user a clean
    interface and all register settings are centralized in one place, device
    tree.
    
    This is already done by DTreg driver in the upstream, DTreg driver is
    preferred by maintainers and will replace this secreg driver once our
    upstream effort for all families has concluded.
    
    In the meantime, we shall keep this secreg driver implementation as
    internal use for our release branches.
    
    Signed-off-by: Tien Fong Chee <tien.fong.chee@altera.com>
    Signed-off-by: Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@altera.com>

--- arch/arm/mach-socfpga/spl_soc64.c
+++ arch/arm/mach-socfpga/spl_soc64.c
@@ -134,6 +134,7 @@ u32 spl_boot_mode(const u32 boot_device)
 /* board specific function prior loading SSBL / U-Boot */
 void spl_perform_fixups(struct spl_image_info *spl_image)
 {
+#if (IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5))
 	int ret;
 	struct udevice *dev;
 
@@ -142,6 +143,9 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 		printf("HPS SMMU secure settings init failed: %d\n", ret);
 		hang();
 	}
+#endif
+	socfpga_init_smmu();
+
 #if !(IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_SIMICS) || IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_EMU))
 	mbox_hps_stage_notify(HPS_EXECUTION_STATE_SSBL);
 #endif
