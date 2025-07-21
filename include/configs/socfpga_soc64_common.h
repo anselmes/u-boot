altera-opensource/u-boot-socfpga

Revert "socfpga64: Do not define CONFIG_SYS_MEM_RESERVE_SECURE to 0"
CONFIG_SYS_MEM_RESERVE_SECURE is required when compiling u-boot with non ATF.

Signed-off-by: Yau Wai Gan <yau.wai.gan@intel.com>

include/configs/socfpga_soc64_common.h
@@ -34,6 +34,7 @@
 /* Magic word to indicate L2 reset is completed */
 #define L2_RESET_DONE_STATUS		0x1228E5E7
 #define CONFIG_SYS_CACHELINE_SIZE	64
+#define CONFIG_SYS_MEM_RESERVE_SECURE	0	/* using OCRAM, not DDR */
 
 /*
  * U-Boot console configurations

anselmes/u-boot

include/configs/socfpga_soc64_common.h
@@ -31,6 +31,7 @@
 #define L2_RESET_DONE_REG		0xFFD12218
 /* Magic word to indicate L2 reset is completed */
 #define L2_RESET_DONE_STATUS		0x1228E5E7
+#define CFG_SYS_MEM_RESERVE_SECURE	0	/* using OCRAM, not DDR */
 
 /*
  * U-Boot console configurations