altera-opensource/u-boot-socfpga

arm: socfpga: agilex_edge: Add emu flag
This patch temporary add the emu flag to bypass
some functions that are not working currently.
It shall be updated and removed in future.

Signed-off-by: Yau Wai Gan <yau.wai.gan@intel.com>

arch/arm/mach-socfpga/board.c
@@ -135,7 +135,7 @@ int board_fit_config_name_match(const char *name)
 {
 	char board_name[10];
 
-#if IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX_EDGE_SIMICS)
+#if IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX_EDGE_SIMICS) || IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX_EDGE_EMU)
 	sprintf(board_name, "board_%u", 0); /* Hardcoded board ID since Simics no support */
 #else
 	sprintf(board_name, "board_%u", socfpga_get_board_id());

arch/arm/mach-socfpga/misc.c
@@ -155,9 +155,7 @@ void socfpga_fpga_add(void *fpga_desc)
 
 int arch_cpu_init(void)
 {
-#if !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX_EDGE_SIMICS)
 	socfpga_get_managers_addr();
-#endif
 
 #ifdef CONFIG_HW_WATCHDOG
 	/*
@@ -166,7 +164,7 @@ int arch_cpu_init(void)
 	 * timeout value is still active which might too short for Linux
 	 * booting.
 	 */
-#if !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX_EDGE_SIMICS)
+#if !(IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX_EDGE_SIMICS) || IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX_EDGE_EMU))
 	hw_watchdog_init();
 #endif
 #else

arch/arm/mach-socfpga/secure_reg_helper.c
@@ -18,7 +18,7 @@
 int socfpga_secure_convert_reg_id_to_addr(u32 id, phys_addr_t *reg_addr)
 {
 	switch (id) {
-#if !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX_EDGE_SIMICS)
+#if !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX_EDGE)
 	case SOCFPGA_SECURE_REG_SYSMGR_SOC64_SDMMC:
 		*reg_addr = socfpga_get_sysmgr_addr() + SYSMGR_SOC64_SDMMC;
 		break;

anselmes/u-boot

arch/arm/mach-socfpga/board.c
@@ -148,7 +148,7 @@ int board_fit_config_name_match(const char *name)
 {
 	char board_name[10];
 
-#if IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_SIMICS)
+#if IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_SIMICS) || IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_EMU)
 	sprintf(board_name, "board_%u", 0); /* Hardcoded board ID since Simics no support */
 #else
 	sprintf(board_name, "board_%u", socfpga_get_board_id());

arch/arm/mach-socfpga/misc.c
@@ -168,9 +168,7 @@ void socfpga_fpga_add(void *fpga_desc)
 
 int arch_cpu_init(void)
 {
-#if !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_SIMICS)
 	socfpga_get_managers_addr();
-#endif
 
 #ifdef CONFIG_HW_WATCHDOG
 	/*
@@ -179,7 +177,7 @@ int arch_cpu_init(void)
 	 * timeout value is still active which might too short for Linux
 	 * booting.
 	 */
-#if !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_SIMICS)
+#if !(IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_SIMICS) || IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_EMU))
 	hw_watchdog_init();
 #endif
 #else

arch/arm/mach-socfpga/secure_reg_helper.c
@@ -17,7 +17,7 @@
 int socfpga_secure_convert_reg_id_to_addr(u32 id, phys_addr_t *reg_addr)
 {
 	switch (id) {
-#if !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5_SIMICS)
+#if !IS_ENABLED(CONFIG_TARGET_SOCFPGA_AGILEX5)
 	case SOCFPGA_SECURE_REG_SYSMGR_SOC64_SDMMC:
 		*reg_addr = socfpga_get_sysmgr_addr() + SYSMGR_SOC64_SDMMC;
 		break;